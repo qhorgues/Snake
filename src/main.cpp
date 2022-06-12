409a3ae530ea96e9dedca96b091ea40f38fac240
qhorgues <quentin.horgues@outlook.fr>
2022-06-12 09:59:54 +0200

    Test du Snake

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
+#include <Snake.hpp>
 
 int main()
 {
+    Snake snake {100, 100};
+    snake.setDirection(-1, 0);
+    snake.addOnMoveElement(5);
+    snake.move();
+    snake.move();
+    snake.move();
+    snake.move();
+    snake.move();
+    for (Point2 const & e : snake)
+    {
+        std::cout << e.x << '/' << e.y << std::endl;
+    }
     return 0;
 }
