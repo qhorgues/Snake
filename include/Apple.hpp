bf36ee7add723f8c309c29a6915ac8807354e1fb
Quentin Horgues <quentin.horgues@outlook.fr>
2022-06-23 15:43:40 +0200

    finalisation de Apple avec ajout du spawn random et la recuperation de sa position

--- include/Apple.hpp
+++ include/Apple.hpp
@@ -1,10 +1,13 @@
 #ifndef APPLE_HPP
 #define APPLE_HPP
 
+#include "Point2.hpp"
+
 class Apple final
 {
 public:
-    Apple() = default;
+    Apple() = delete;
+    Apple(Point const width_map, Point const height_map);
     ~Apple() = default;
 
     Apple(Apple const &) = delete;
@@ -13,9 +16,11 @@ public:
     Apple(Apple &&) = delete;
     Apple& operator=(Apple &&) = delete;
 
-private:
-    
+    void spawnApple(Point const width_map, Point const height_map);
+    Point2 const & getPosition() const noexcept;
 
-}
+private:
+    Point2 m_position;
+};
 
 #endif
--- src/Apple.cpp
+++ src/Apple.cpp
@@ -1,2 +1,28 @@
 #include "../include/Apple.hpp"
+#include <random>
+#include <tuple>
 
+Apple::Apple(Point const width_map, Point const height_map)
+{
+    spawnApple(width_map, height_map);
+}
+
+void Apple::spawnApple(Point const width_map, Point const height_map)
+{
+    std::tuple<Point, Point> const width {width_map/20,  width_map - width_map/20};
+    std::tuple<Point, Point> const height {height_map/20,  height_map - height_map/20};
+
+
+    std::random_device rd;
+    std::mt19937 gen{rd()};
+    std::uniform_int_distribution<> distrib_width(std::get<0>(width), std::get<1>(width));
+    std::uniform_int_distribution<> distrib_height(std::get<0>(height), std::get<1>(height));
+
+    m_position.x = distrib_width(gen);
+    m_position.y = distrib_height(gen);
+}
+
+Point2 const & Apple::getPosition() const noexcept
+{
+    return m_position;
+}
