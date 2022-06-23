53b35e4c546182c9c8ea45d8ea53ded5a4293b92
Quentin Horgues <quentin.horgues@outlook.fr>
2022-06-23 15:44:37 +0200

    test de la class Apple

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
+#include <iostream>
+#include "../include/Apple.hpp"
 
 int main()
 {
+    Apple apple(250, 200);
+    for (int i = 0; i < 1000; i++)
+    {
+        std::cout << apple.getPosition().x << '/' << apple.getPosition().y << std::endl;
+        apple.spawnApple(250, 200);
+    }
     return 0;
 }
