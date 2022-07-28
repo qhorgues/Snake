235c45bd1697fc32c1f5ed4d75b5c56fd80b02f1
Quentin Horgues <quentin.horgues@outlook.fr>
2022-07-28 13:10:56 +0200

    Suppresion du constructeur prenant l'emplacement de l'executable

--- include/Interface.hpp
+++ include/Interface.hpp
@@ -11,20 +11,21 @@
 class Interface
 {
 public:
-    Interface() = delete;
-	Interface(std::string const &) {}
-    ~Interface() = default;
+    Interface() = default;
+    virtual ~Interface() = default; // On autorise le polymorphisme
 
     Interface(Interface const &) = delete;
     Interface& operator=(Interface const &) = delete;
     Interface(Interface &&) = delete;
     Interface& operator=(Interface &&) = delete;
 
-    virtual void getEvent(GameStatut & statut, GameMode & mode) noexcept = 0;
-    virtual void update(GameStatut const statut, GameMode const mode) = 0;
+    virtual void getEvent(Game::Statut & statut, Game::Mode & mode) noexcept = 0;
+    virtual void update(Game::Statut const statut, Game::Mode const mode) = 0;
     virtual bool isRunning() const = 0;
     virtual std::size_t getSizeSnake() const noexcept = 0;
     virtual std::size_t getSizeApple() const noexcept = 0;
 };
+
+
 
 #endif
