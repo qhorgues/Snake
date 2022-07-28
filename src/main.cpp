91b4119da9e5c27013b162f894467576d0918fb5
Quentin Horgues <quentin.horgues@outlook.fr>
2022-07-28 13:11:48 +0200

    Test de l'interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
+#include "../include/InterfaceSFML.hpp"
+#include "../include/interfaceSFML/TextButton.hpp"
+#include <memory>
 
 int main()
 {
+    std::unique_ptr<Interface> interface { std::make_unique<SFML::Interface>() };
+    Game::Mode mode {Game::Mode::NONE};
+    Game::Statut statut {Game::Statut::MAIN_MENU};
+    
+    while (interface->isRunning())
+    {
+        interface->update(statut, mode);
+        interface->getEvent(statut, mode);
+    }
+
     return 0;
 }
