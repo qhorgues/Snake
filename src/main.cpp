239d74435a91638d1717347801d3b72d3c4a3d05
Quentin Horgues <quentin.horgues@outlook.fr>
2022-08-04 12:09:54 +0200

    ajput event

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,18 @@
 #include "../include/SFML_Interface.hpp"
 #include "../include/SFML_TextButton.hpp"
 
+#include "../include/SFML_Menu.hpp"
+
 int main()
 {
     std::unique_ptr<Interface> interface { std::make_unique<SFML::Interface>() };
-    Game::Mode mode {Game::Mode::NONE};
+    Game::Mode mode {Game::Mode::NO_MODE};
     Game::Statut statut {Game::Statut::MAIN_MENU};
+    Game::Event event {Game::Event::NO_EVENT};
+
     while (interface->isRunning())
     {
-        interface->update(statut, mode);
+        interface->update(statut, mode, event);
         interface->getEvent(statut, mode);
     }
 
