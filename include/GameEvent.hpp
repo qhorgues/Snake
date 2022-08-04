5242a592053063fdc8625c26bf382963a3a29939
Quentin Horgues <quentin.horgues@outlook.fr>
2022-08-04 11:50:25 +0200

    Ajout enum événement en jeu

---
+++ include/GameEvent.hpp
@@ -0,0 +1,16 @@
+#ifndef GAME_EVENT_HPP
+#define GAME_EVENT_HPP
+
+namespace Game
+{
+    enum Event
+    {
+        NO_EVENT,
+        PLAYER_1_WIN,
+        PLAYER_2_WIN,
+        LOSE,
+        EQUALITY
+    };
+}
+
+#endif
