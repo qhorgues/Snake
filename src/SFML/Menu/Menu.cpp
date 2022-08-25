78e1b8548f34ebef3929d7523f722aee93765f92
Quentin Horgues <quentin.horgues@outlook.fr>
2022-08-25 19:12:46 +0200

    la font est devenu membre de l'objet corigeant une segfault

--- src/SFML/Menu/Menu.cpp
+++ src/SFML/Menu/Menu.cpp
@@ -1,39 +1,54 @@
 #include <array>
 #include <stdexcept>
+#include <limits>
+#include <cassert>
 #include "Menu.hpp"
 
 namespace SFML
 {
 
-    static sf::Color const blue_menu{63, 72, 204};
+    static sf::Color const blue_menu {63, 72, 204};
 
-    Menu::Menu(sf::RenderWindow const &window) : m_button()
+    /**
+     * @brief Construit le nouvelle objet Menu
+     * 
+     * @param window l'objet representant la fenetre
+     * 
+     * @exception runtime_error Dans le cas d'echec de chargement de la police arial
+     */
+    Menu::Menu(sf::RenderWindow const &window) : m_button(), m_textFont()
     {
-        sf::Font font;
-        font.loadFromFile("assets/arial.ttf");
-        m_button = createMap(window, font, {});
+        if (!m_textFont.loadFromFile("assets/arial.ttf"))
+        {
+            throw std::runtime_error("Impossible to load font arial");
+        }
+        m_button = createMap(window, m_textFont, {});
     }
 
     Menu::Menu(sf::RenderWindow const & window, std::initializer_list<std::string> const & list_button) : m_button()
     {
-        sf::Font font;
-        font.loadFromFile("assets/arial.ttf");
-        m_button = createMap(window, font, list_button);
+        if (!m_textFont.loadFromFile("assets/arial.ttf"))
+        {
+            throw std::runtime_error("Impossible to load font arial");
+        }
+        m_button = createMap(window, m_textFont, list_button);
     }
 
     Menu::mapButton Menu::createMap(sf::RenderWindow const &window, sf::Font const &font, std::initializer_list<std::string> const &list_button)
     {
         mapButton button;
+        assert(std::size(list_button) > static_cast<size_t>(std::numeric_limits<int>::max()) && "list button is to large");
+        int const numberButton { static_cast<int>(std::size(list_button)) };
         sf::Vector2u const windowSize{window.getSize()};
-        sf::Vector2i const buttonSize{static_cast<int>(windowSize.x) / 3, static_cast<int>(windowSize.y) / 3};
+        sf::Vector2i const buttonSize{static_cast<int>(windowSize.x) / 3, static_cast<int>(static_cast<double>(windowSize.y) / static_cast<double>(std::size(list_button)+1LL)) };
 
         int index = 0;
         for (std::string const &key : list_button)
         {
             if (button.find(key) == button.end())
             {
-                sf::Vector2i const positionButton{static_cast<int>(windowSize.x) / 3 - buttonSize.x / 3, (static_cast<int>(windowSize.y) / 3) * (index + 1) - buttonSize.y / 3};
-                button.insert_or_assign(key, SFML::TextButton{key, positionButton, buttonSize, sf::Color::White, blue_menu, font, windowSize.x / 40});
+                sf::Vector2i const positionButton{static_cast<int>(windowSize.x) / 4 - buttonSize.x / 2, (static_cast<int>(windowSize.y) / static_cast<int>(std::size(list_button))) * (index + 1) - buttonSize.y};
+                button.insert_or_assign(key, SFML::TextButton{key, positionButton, buttonSize, sf::Color::White, blue_menu, font, windowSize.x / 20});
                 index++;
             }
         }
--- src/SFML/Menu/Menu.hpp
+++ src/SFML/Menu/Menu.hpp
@@ -15,26 +15,29 @@ namespace SFML
     {
     public:
         Menu() = delete;
-        Menu(sf::RenderWindow const & window);
-        Menu(sf::RenderWindow const & window, std::initializer_list<std::string> const & list_button);
+        Menu(sf::RenderWindow const &window);
+        Menu(sf::RenderWindow const &window, std::initializer_list<std::string> const &list_button);
         virtual ~Menu() = default;
 
         Menu(Menu const &) = default;
-        Menu & operator=(Menu const &) = default;
+        Menu &operator=(Menu const &) = default;
 
         Menu(Menu &&) = default;
-        Menu & operator=(Menu &&) = default;
+        Menu &operator=(Menu &&) = default;
 
-        virtual void update(sf::RenderWindow & window, Game::Event const event) const = 0;
-        virtual void getEvent(Game::Statut & statut, Game::Mode & mode) const = 0;
-        void drawButton(sf::RenderWindow & window, std::string const & name_button) const;
-        bool pointCollideWithButton(std::string const & name_button, sf::Vector2i const & point) const;
+        virtual void update(sf::RenderWindow &window, Game::Event const event) const = 0;
+        virtual void getEvent(Game::Statut &statut, Game::Mode &mode) const = 0;
+
+    protected:
+        void drawButton(sf::RenderWindow &window, std::string const &name_button) const;
+        bool pointCollideWithButton(std::string const &name_button, sf::Vector2i const &point) const;
 
     private:
         using mapButton = std::unordered_map<std::string, SFML::TextButton>;
-        static mapButton createMap(sf::RenderWindow const & window, sf::Font const & font, std::initializer_list<std::string> const & list_button);
+        static mapButton createMap(sf::RenderWindow const &window, sf::Font const &font, std::initializer_list<std::string> const &list_button);
 
         mapButton m_button;
+        sf::Font m_textFont;
     };
 }
 
